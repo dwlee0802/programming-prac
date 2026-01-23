a85c739ca07d28df56183527293db199fe8bab25
dwl <dwlee0802@gmail.com>
2026-01-23 10:48:45 -0800

    10718

---
+++ bronze/10718.cpp
@@ -0,0 +1,9 @@
+// https://www.acmicpc.net/problem/10718
+
+#include <iostream>
+
+int main(int argc, char **args)
+{
+	std::cout << "강한친구 대한육군" << std::endl;
+	std::cout << "강한친구 대한육군" << std::endl;
+}

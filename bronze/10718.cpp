89e16bcc3827d03fa1c3363e3133a0b1e764629f
dwl <dwlee0802@gmail.com>
2026-01-23 11:00:42 -0800

    deleted duplicate files

--- bronze/10718.cpp
+++
@@ -1,9 +0,0 @@
-// https://www.acmicpc.net/problem/10718
-
-#include <iostream>
-
-int main(int argc, char **args)
-{
-	std::cout << "강한친구 대한육군" << std::endl;
-	std::cout << "강한친구 대한육군" << std::endl;
-}
